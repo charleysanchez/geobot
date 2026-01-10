1e495cd0c7a667e78ba483f7b1bedd5234762489
Charley Sanchez <charleysanchez7@gmail.com>
2026-01-10 14:28:52 -0500

    updated structure of dataset to work

--- scripts/organize_dataset.py
+++ scripts/organize_dataset.py
@@ -1,12 +1,19 @@
 import os
 
 def create_image_dir(root="dataset"):
-    os.mkdir(f"{root}/images")
-    os.mkdir(f"{root}/coords")
+    img_path = os.path.join(root, "images")
+    coords_path = os.path.join(root, "coords")
+    if not os.path.exists(img_path):
+        os.mkdir(img_path)
+    
+    if not os.path.exists(coords_path):
+        os.mkdir(coords_path)
 
-    for path in os.listdir(root):
-        if path.endswith(".png"):
-            print(path)
+    for file in os.listdir(root):
+        if file.endswith(".png"):
+            os.rename(os.path.join(root, file), os.path.join(img_path, file))
+        elif file.endswith(".csv"):
+            os.rename(os.path.join(root, file), os.path.join(coords_path, file))
 
 
 if __name__ == "__main__":
