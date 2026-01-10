b3ce423e0f1c45bf573c9b0b5ef2e1ba86f405fe
Charley Sanchez <charleysanchez7@gmail.com>
2026-01-10 15:05:50 -0500

    dataset passes tests

--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,6 @@
 from torch.utils.data import Dataset
+from torchvision import transforms
+import torch
 from PIL import Image
 import csv
 import os
@@ -11,13 +13,14 @@ class ImageToGeoDataset(Dataset):
         self.img_files = sorted([
             f for f in os.listdir(self.img_dir)
             if f.endswith(".png")
-        ])
+        ], key=lambda x: int(os.path.splitext(x)[0]))
 
         self.coords = []
         with open(f"{self.coords_dir}/coords.csv", "r") as f:
             reader = csv.reader(f)
-            self.coords = list(reader)
+            self.coords = [tuple(map(float, row)) for row in reader]
 
+        print(self.coords)
         self.img_transform = img_transform
 
     def __len__(self):
@@ -27,4 +30,31 @@ class ImageToGeoDataset(Dataset):
         img_name = self.img_files[idx]
 
         img_path = os.path.join(self.img_dir, img_name)
+        image = Image.open(img_path).convert("RGB")
+        coords = self.coords[idx]
+
+        return (
+            self.img_transform(image) if self.img_transform else image,
+            torch.tensor(coords, dtype=torch.float32)
+        )
+    
+if __name__ == "__main__":
+    transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),   # <-- converts PIL → torch.Tensor (C,H,W)
+        ])
+    dataset = ImageToGeoDataset(
+        root="dataset",
+        img_transform=transform
+    )
+
+    print("Dataset length:", len(dataset))
+    image, coords = dataset[0]
+
+    print(type(image), image.shape)
+    print(type(coords), coords.shape, coords, coords.dtype)
+
+    for i in range(5):
+        img, c = dataset[i]
+        print(i, dataset.img_files[i], c.tolist())
         
