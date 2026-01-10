e8097ee3878823337809a62f4d3e635d23f04acb
Charley Sanchez <charleysanchez7@gmail.com>
2026-01-10 14:55:30 -0500

    updating dataset to work with new structure

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # geobot
 Deep Learning model trained on Google Streetview images from around the world to pinpoint global coordinates based on image.
 
+## Instructions
+
+1. ```curl -L -o ~/Downloads/google-street-view.zip\
+  https://www.kaggle.com/api/v1/datasets/download/paulchambaz/google-street-view && mv ~/Downloads/google-street-view.zip . && unzip google-street-view.zip
+  ```
+
+2. Run `scripts/organize_dataset.py`
 
 ## Dataset
 [Kaggle Page](https://www.kaggle.com/datasets/paulchambaz/google-street-view/data)
--- src/dataset.py
+++ src/dataset.py
@@ -1,12 +1,30 @@
+from torch.utils.data import Dataset
+from PIL import Image
+import csv
 import os
 
-class ImageDataset():
-    def __init__(self, root="dataset"):
-        self.root = root
+class ImageToGeoDataset(Dataset):
+    def __init__(self, root="dataset", img_transform=None):
+        self.img_dir = os.path.join(root, "images")
+        self.coords_dir = os.path.join(root, "coords")
 
-    def get_dataset(self):
-        imgs = {}
-        contents = os.listdir(self.root)
-        for i, path in enumerate(contents):
-            if path.endswith("png"):
-                imgs[i] = path
+        self.img_files = sorted([
+            f for f in os.listdir(self.img_dir)
+            if f.endswith(".png")
+        ])
+
+        self.coords = []
+        with open(f"{self.coords_dir}/coords.csv", "r") as f:
+            reader = csv.reader(f)
+            self.coords = list(reader)
+
+        self.img_transform = img_transform
+
+    def __len__(self):
+        return len(self.img_files)
+
+    def __getitem__(self, idx):
+        img_name = self.img_files[idx]
+
+        img_path = os.path.join(self.img_dir, img_name)
+        
